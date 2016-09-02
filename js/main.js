c786650fad20e7c5b333fa1b9f4a3d49f4e24e3d
mr.cao <340395573@qq.com>
2016-09-02 17:19:58 +0800

    Site updated: 2016-09-02 17:19:57

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ require([], function (){
 
 	var isMobileInit = false;
 	var loadMobile = function(){
-		require([yiliaConfig.root + '/js/mobile.js'], function(mobile){
+		require(['/js/mobile.js'], function(mobile){
 			mobile.init();
 			isMobileInit = true;
 		});
 	}
 	var isPCInit = false;
 	var loadPC = function(){
-		require([yiliaConfig.root + '/js/pc.js'], function(pc){
+		require(['/js/pc.js'], function(pc){
 			pc.init();
 			isPCInit = true;
 		});
@@ -55,7 +55,7 @@ require([], function (){
 
 	//是否使用fancybox
 	if(yiliaConfig.fancybox === true){
-		require([yiliaConfig.root + '/fancybox/jquery.fancybox.js'], function(pc){
+		require(['/fancybox/jquery.fancybox.js'], function(pc){
 			var isFancy = $(".isFancy");
 			if(isFancy.length != 0){
 				var imgArr = $(".article-inner img");
